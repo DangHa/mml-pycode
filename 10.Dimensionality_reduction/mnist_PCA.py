7c8c1a078e807d7e503def419a5bc3b0a9872083
dangha <dangha997@gmail.com>
2021-07-27 19:24:12 +0700

    mnist PCA

--- 10.Dimensionality_reduction/mnist_PCA.py
+++ 10.Dimensionality_reduction/mnist_PCA.py
@@ -8,7 +8,9 @@ import torchvision.datasets as datasets
 
 from torchvision import datasets, transforms
 
-img_of_number = 2
+img_of_number = 8
+batch_size = 1000 # need to be > 4 coz we show 4 images below
+PCs = 100
 
 ### Step 1 + 2: Normalization
 # Those images are described by 28x28 dimensions => normalizing with mean = 0.5 and std = 0.5
@@ -24,7 +26,7 @@ mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transf
 idx = mnist_trainset.targets == img_of_number
 mnist_trainset.targets = mnist_trainset.targets[idx]
 mnist_trainset.data = mnist_trainset.data[idx]
-trainloader = torch.utils.data.DataLoader(mnist_trainset, batch_size = 3) 
+trainloader = torch.utils.data.DataLoader(mnist_trainset, batch_size = batch_size) 
 
 # Testing dataset
 mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
@@ -32,21 +34,22 @@ mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transf
 idx = mnist_testset.targets == img_of_number
 mnist_testset.targets = mnist_testset.targets[idx]
 mnist_testset.data = mnist_testset.data[idx]
-testloader = torch.utils.data.DataLoader(mnist_testset, batch_size = 16) # 16 images / 1 tensor
+testloader = torch.utils.data.DataLoader(mnist_testset, batch_size = batch_size) # 16 images / 1 tensor
 
-# get 1 batch of data
+## get 1 batch of data
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
-# plt.imshow(images[0].numpy().squeeze())
-    
-## change images 28x28 to vectors 784 dimenstions
-X = []
-for img in images:
-    X.append(img.numpy().reshape(784,))
+# axarr[k].imshow(images[k].numpy().squeeze())
 
-X = np.asmatrix(X).T
-print("X:", X.shape)
+## change images 28x28 to vectors 784 dimenstions
+def img_to_vector(images):
+    X = []
+    for img in images:
+        X.append(img.numpy().reshape(784,))
+
+    X = np.asmatrix(X).T
+    return X
 
 
 ### Finding eigenvectors of the covariance matrix (Using SVD)
@@ -61,19 +64,34 @@ def SVD(X):
     
     return λ, B
 
-λ, B = SVD(X)
 
 ### Projection 
 def projection(B, λ, X):
-    
+    # get only the eigenvectors have the highest eigenvalues
+    B = B[:,0:PCs]
+
     Z = B.T*X
 
     X_hat = B*Z
 
-    print("X_har: ", X_hat.shape)
-    print(X_hat[:,0].reshape(28,28).shape)
-    plt.imshow(X_hat[:,0].reshape(28,28))
+    return X_hat
 
 
-projection(B, λ, X)
+############ Run ############
+X = img_to_vector(images)
+λ, B = SVD(X)
+X_hat = projection(B, λ, X)
+
+
+f, axarr = plt.subplots(1,4) 
+
+for k in range(4):
+    axarr[k].imshow(X_hat[:,k].reshape(28,28))
+    
+    # remove x,y axises
+    axarr[k].axes.get_yaxis().set_visible(False)
+    axarr[k].axes.get_xaxis().set_visible(False)
+
+
+plt.title("PCs = " + str(PCs), loc = 'left', color = "blue")
 plt.show()
