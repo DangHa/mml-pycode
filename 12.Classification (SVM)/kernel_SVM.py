286fa6ef5c8fa5e4579ccaaf3985097512ab361b
dangha <dangha997@gmail.com>
2021-08-13 21:53:15 +0700

    kernel SVM

--- 12.Classification (SVM)/kernel_SVM.py
+++ 12.Classification (SVM)/kernel_SVM.py
@@ -1,6 +1,4 @@
-# Linear - Support vector machine (Linear - SVM)
-# other instruction
-# https://github.com/CihanBosnali/Support-Vector-Machine-without-ML-libraries/blob/master/Support_Vector_Machine_from_Scratch.ipynb
+# Non_linear - Support vector machine (Non_linear - SVM)
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +7,7 @@ import torch
 from sklearn import svm
 
 
-number_of_point = 150
+number_of_point = 100
 
 ### Generating data inside a circle
 def gererating_data(size):
@@ -23,7 +21,7 @@ def gererating_data(size):
     Y1 = np.full((size, 1), 1)
 
     # Second cluster, y = -1
-    X21 = torch.empty(size).normal_(mean=10,std=2).numpy().reshape(size, 1)
+    X21 = torch.empty(size).normal_(mean=5,std=2).numpy().reshape(size, 1)
     X22 = torch.empty(size).normal_(mean=0,std=2).numpy().reshape(size, 1)
     plt.plot(X21.T, X22.T, color='blue', marker='_')
     
@@ -37,32 +35,36 @@ X1, X2, Y1, Y2 = gererating_data(int(number_of_point/2))
 X = np.vstack((X1, X2))
 Y = np.vstack((Y1, Y2)).reshape(number_of_point)
 
-# SVM - linear
-clf = svm.SVC(kernel='linear', random_state = 1)
+# SVM - kernel rbf
+clf = svm.SVC(kernel='rbf', random_state = 1)
 clf.fit(X,Y)
 
 
 ############ Visualization ############
-# get the end of x,y of graph
+# get the x,y of graph
 ax = plt.gca()
 xlim = ax.get_xlim()
 ylim = ax.get_ylim()
 
 # create grid to evaluate model
-xx = np.linspace(xlim[0], xlim[1], 30)
-yy = np.linspace(ylim[0], ylim[1], 30)
-YY, XX = np.meshgrid(yy, xx)
-xy = np.vstack([XX.ravel(), YY.ravel()]).T
+xx, yy = np.meshgrid(np.linspace(xlim[0], xlim[1], 500),
+                     np.linspace(ylim[0], ylim[1], 500))
+xy = np.vstack([xx.ravel(), yy.ravel()]).T
 
 
 # plot decision boundary and margins
 # decision_function gives the distences between the points and SVM-hyperplane
-Z = clf.decision_function(xy).reshape(XX.shape)
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()), aspect='auto',
+           origin='lower', cmap='viridis')
 
 # XX, YY is coordinate of contour
 # Z is coordinate of 1 points to the origin point => its a line
 # with Z = x^2 + y^2 => its a circle
-ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1], alpha=0.5,
+ax.contour(xx, yy, Z, colors='k', levels=[-1, 0, 1], alpha=0.5,
            linestyles=['--', '-', '--'])
 
 # plot support vectors - the points lies on the margin
